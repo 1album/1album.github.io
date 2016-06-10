eb99da461c6f7b5742b65a9b0352d07ec7e04e60
Stefan Hong <stefan@1.digital>
2016-06-10 11:15:07 +0800

    add windows docs

--- en/js/docs.js
+++ en/js/docs.js
@@ -27,6 +27,19 @@ $(function() {
     } else {
         anchors.parentElement.removeChild(anchors)
     }
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+  });
 });
 
 $('.menu-body').on('show.bs.collapse', function(){
